200ba9a3f15bb6477d5d2d0bface3650ba392fea
Anupam0173 <anupam.kushwaha@thinkitive.com>
2021-09-08 06:24:34 +0530

    adding of cors middleware

---
+++ app.js
@@ -0,0 +1,35 @@
+var express=require('express');
+let cors = require("cors");
+var app=express();
+var bodyparser=require('body-parser');
+const dbconnection=require('./db_connection/dbConnection');
+
+// All Api url
+const restaurant_route=require("./routes/restaurant_route");
+const product_route=require("./routes/product_route");
+
+// adding middleware -cors
+app.use(cors());
+
+dbconnection()
+//body parser for sending & recieving the data in json format
+app.use(bodyparser.json());
+
+// for sending and recieving the data via form-data format
+app.use(bodyparser.urlencoded());
+
+
+// Handling routes request
+app.use("/restaurant",restaurant_route);
+app.use("/product",product_route);
+
+
+// testing server
+app.get('/',(req,res)=>{
+    res.send("foobar");
+});
+
+const port=process.env.PORT||3003;
+app.listen(port,()=>{
+    console.log("server started at port:"+port);
+});
